c43cf5de6bb76cadccbdf9637f12184865eb3a44
Ali Ahmed <alyahmedrbk@gmail.com>
2025-06-18 19:03:42 +0500

    Fix Terraform format

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"  
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
   tags = {
