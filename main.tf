bcb7606a3ee7c3bbc7d661738d92e74283379aff
Aly Ahmed <alyahmedrbk@gmail.com>
2025-06-18 19:26:09 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-09e6f87a47903347c"
   instance_type = "t2.micro"
 
   tags = {
